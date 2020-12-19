dddfdec30ea9cfe43480f8ac0b5a82ca74784663
Himanshu Bhatt <56117332+himanshubhatt12@users.noreply.github.com>
2020-12-19 20:57:20 +0530

    added log and exponents

---
+++ math/log_exp.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	// double exp (double x);
+	int n = 2;
+	cout<<exp(2)<<endl;
+
+	//  logarithm
+	//  double log (double x);
+	int x = 5;
+	cout<<log(x)<<endl;
+
+	double y = 30.4;
+	cout<<log(y)<<endl;
+
+	//  logarithm to base 10
+	//  double log10 (double x);
+	cout<<log10(x)<<endl;
+	cout<<log10(y)<<endl;
+
+	//  logarithm to base 2
+	//  double log2 (double x);
+	cout<<log2(x)<<endl;
+	cout<<log2(y)<<endl;
+	
+	return 0;
+}
